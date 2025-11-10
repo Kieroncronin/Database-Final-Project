40438eced42a1099a0875191fffe2e150dfec33e
zmoore001-blip <zmoore001@converse.edu>
2025-11-10 10:24:53 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.post('/', (req, res) => {
+  const id = req.body.id;
+  const role = req.body.role;
+
+  if (role === 'student') {
+    res.redirect('/student?id=' + id);
+  } else if (role === 'faculty') {
+    res.redirect('/faculty?id=' + id);
+  } else if (role === 'registrar') {
+    res.redirect('/registrar?id=' + id);
+  } else {
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
