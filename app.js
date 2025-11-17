db8f174c8b1e14e1695f8d95011ff5160ed65d5f
Kieron Cronin <kieronjamescronin@gmail.com>
2025-11-17 14:34:26 -0500

    pls work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
+const sql3 = require('sqlite3').verbose();
 
 app.set('view engine', 'pug');
 app.use(express.urlencoded({extended: true}));
@@ -9,16 +10,29 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+const db = new sql3.Database('./univdb.sqli');
+
 app.post('/display', (req, res) => {
     const {id, role} = req.body;
-    if (role === 'teacher'){
-        res.render('teacher', {id});
+    if (role === 'faculty'){
+        db.get("SELECT * FROM Faculty WHERE FacSSN = ?", [id], (err, row) =>{
+            if (err) return res.send("Error: " + err.message);
+            if (!row) return res.send("Faculty ID not found");
+            res.render('faculty', { faculty: row });
+        });
     }
-    else if (role === 'admin'){
-        res.render('admin', {id});
+    else if (role === 'registrar'){
+        res.render('registrar', {id});
+    }
+    else if (role === 'student'){
+        db.get("SELECT * FROM Student WHERE StdSSN = ?", [id], (err, row) =>{
+            if (err) return res.send("Error: " + err.message);
+            if (!row) return res.send("Student ID not found");
+            res.render('student', { student: row });
+        });
     }
     else {
-        res.render('student', {id});
+        res.send("Error")
     }
 });
 
